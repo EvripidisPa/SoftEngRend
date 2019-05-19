128c4c71f6fa68a2cfd6304893ad64762cf3914a
Tsafaras <49597095+Tsafaras@users.noreply.github.com>
2019-05-19 20:40:01 +0300

    store addition

--- SoftEngRend/Store.cpp
+++ SoftEngRend/Store.cpp
@@ -1,95 +1,123 @@
-#include "stdafx.h"
+#include <iostream>
+#include <string>
 #include "Store.h"
 
 
-Store::Store(int ID, string name, double latitude, double longitude, Owner * owner)
-{
-	this->ID = ID;
-	this->name = name;
-	this->latitude = latitude;
-	this->longitude = longitude;
+using namespace std;
 
-	if (owner != nullptr)
-		this->ptr_owner = owner;
+Store::Store(int I_D, string Name, double Latitude, double Longitude, Owner* OWN)
+{
+	ID = I_D;
+	name = Name;
+	latitude = Latitude;
+	longitude = Longitude;
+	if (Owner != nullptr)
+	{
+		owner = OWN;
+	}
 }
 
-Store::Store(int ID, string name, Owner * owner)
+Store::Store(int I_D, string Name, Owner* OWN)
 {
-	this->ID = ID;
-	this->name = name;
-	this->ptr_owner = owner;
+	ID = I_D;
+	name = Name;
+	Owner = OWN;
 }
 
-Store::~Store()
+string Store::getBio() const { return bio; }
+string Store::getName() const { return name; }
+int Store::getID() const { return ID; }
+double Store::getLat() const { return latitude; }
+double Store::getLong() const { return longitude; }
+int Store::getCapacity() const { return capacity; }
+int Store::getCapacity_Park() const { return capacity_park; }
+float Store; :getRating() const { return rating; }
+
+
+void Store::setBio(string bio);
+void Store::getName(string name);
+void Store::setLat(double latitude);
+void Store::setLongitude(double longitude);
+void Store::setCapacity(int capacity);
+void Store::setCapacity_Park(int capacity_park);
+void Store::setRating(float rating);
+
+void Store::updateEvent(const Events& katEvent)
 {
+	Event.push_back(katEvent);
+	for (int i = 0; i < Event.size(); i++)
+		cout << Event[i] << '\n';
+}
+const Events Store::getEvents(int e) const
+{
+
+	return Event.at(e);
+
+}
+void Store::deleteEvent()
+{
+	Event.clear();
 }
 
-string Store::getBio()
+void Store::setReservation(const string & katReserv)
 {
-	return string(bio);
+	Reservation.push_back(katReserv);
+	for (int i = 0; i < Reservation.size(); i++)
+		cout << Reservation[i] << '\n';
+}
+const string Store::getReservation(int r) const
+{
+
+	return Reservation.at(r);
+
 }
 
-string Store::getName()
+void Store::updateMenu(const Menu & katMenu)
 {
-	return string(name);
+	menu.push_back(katMenu);
+	for (int i = 0; i < menu.size(); i++)
+		cout << menu[i] << '\n';
+}
+void Store::deletemenu()
+{
+	menu.clear();
 }
 
-int Store::getID()
+void Store::updatePhotos(const Photos & katPhoto);
 {
-	return ID;
+	photos.push_back(katPhoto);
+	for (int i = o; i < photos.size(); i++)
+		cout << photos[i] << '\n';
+}
+const Photos Store::getPhotos(int p) const
+{
+	if (!photos.empty())
+		return photos.at(p);
+	else
+		std::cout << "no photos...\n";
 }
 
-double Store::getLat()
+
+void Store::newmenu(const Menu & katMenu)
 {
-	return latitude;
+	menu.push_back(katMenu);
+	for (int i = 0; i < Menu.size(); i++)
+		cout << Menu[i] << '\n';
+}
+void MenuComp::deleteMenu()
+{
+	Menu.clear();
 }
 
-double Store::getLong()
+void Store::setHours(const DateTime & katHours)
 {
-	return longitude;
+	hours.push_back(katHours);
+	for (int i = 0; i < hours.size(); i++)
+		cout << hours[i] << '\n';
 }
-
-int Store::getCapacity()
+void Store::getHours()
 {
-	return capacity;
-}
-
-int Store::getCapacity_Park()
-{
-	return capacity_park;
-}
-
-float Store::getRating()
-{
-	return rating;
-}
-
-void Store::setBio(string bio)
-{
-	this->bio = bio;
-}
-
-void Store::setLat(double latitude)
-{
-	this->latitude = latitude;
-}
-
-void Store::setLongitude(double longitude)
-{
-	this->longitude = longitude;
-}
-
-void Store::setCapacity(int capacity)
-{
-	this->capacity = capacity;
-}
-
-void Store::setCapacity_Park(int capacity_park)
-{
-	this->capacity_park = capacity_park;
-}
-
-void Store::setRating(float rating)
-{
-	this->rating = rating;
+	for (auto i = hours.begin(); i != hours.end(); ++i)
+		std::cout << *i << ' ';
+	cout << '\n';
 }
--- SoftEngRend/Store.h
+++ SoftEngRend/Store.h
@@ -1,55 +1,67 @@
-#pragma once
+#ifndef STORE_H
+#define STORE_H
 
 #include <string>
-#include "our_structs.h"
 #include "Owner.h"
+#include "our_structs.h"
+#include "stdafx.h"
+#include "Events.h"
+#include "Photo.h"
+
 using namespace std;
 
 class Store
 {
+public:
+	Store(int ID, string name, double latitude, double longitude, Owner* owner);
+	Store(int ID, string name, Owner* owner);
+	string getBio() const;
+	string getName() const;
+	int getID() const;
+	double getLat() const;
+	double getLong() const;
+	int getCapacity() const;
+	int getCapacity_Park() const;
+	float getRating() const;
+
+	void setBio(string bio);
+	void setLat(double latitude);
+	void setLongitude(double longitude);
+	void setCapacity(int capacity);
+	void setCapacity_Park(int capacity_park);
+	void setRating(float rating);
+
+	void updateEvent(const Events&);
+	const Events getEvents(int) const;
+	void deleteEvent();
+
+	void setReservation(const string&);
+	const string getReservation(int) const;
+
+	void updateMenu(const Menu&);
+	void deletemenu();
+
+	void updatePhotos(const Photos&);
+	const Photos getPhotos(int) const;
+
+	void Store::setHours(const DateTime& katHours);
+	const DateTime Store::getHours() const;
+
 private:
 	string name, bio;
 	int ID, capacity, capacity_park;
 	double latitude, longitude;
 	float rating;
 	struct availability;
-	enum status { OPEN = 0 , CLOSED = 1 };
-	Owner *ptr_owner;
+	enum status { OPEN, CLOSED };
+	Owner* owner;
 
-public:
-	Store(int ID, string name, double latitude, double longitude, Owner *ptr_owner);
-
-	Store(int ID, string name, Owner *ptr_owner);
-
-	virtual ~Store();
-
-	string getBio();
-
-	string getName();
-
-	int getID();
-
-	double getLat();
-
-	double getLong();
-
-	int getCapacity();
-
-	int getCapacity_Park();
-
-	float getRating();
-
-	//Setters
-	void setBio(string bio);
-
-	void setLat(double latitude);
-
-	void setLongitude(double longitude);
-
-	void setCapacity(int capacity);
-
-	void setCapacity_Park(int capacity_park);
-
-	void setRating(float rating);
+	std::vector<Events> Event;
+	std::vector<string> Reservation;
+	std::vector<Menu> menu;
+	std::vector<Photos> photos;
+	std::vector<DateTime> hours;
 };
 
+
+#endif // STORE_H
